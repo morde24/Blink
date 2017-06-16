a1e4a2c3e4574aeaa5255a0d6b5bf4bf46b7d9d2
morde24 <momogoyankees@gmail.com>
2017-06-16 10:15:07 -0400

    Blink program

---
+++ Blink.ino
@@ -0,0 +1,16 @@
+int LED = 13;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (13, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite (13, HIGH);
+  Serial.println ("LED is ON");
+  delay(100);
+  digitalWrite (13, LOW);
+  Serial.println ("LED is OFF");
+  delay(100);
+}
